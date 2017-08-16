1bf4d16fe61fee9823ae16864dfa7ae31b706e8c
KyTn <angel.kytn@gmail.com>
2017-08-16 13:52:21 +0200

    unity purchase actualizado, escena para probar las animaciones de los objetos

---
+++ Assets/Scripts/SurvivalSumo2/ChangeItemsOnPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeItemsOnPlayer : MonoBehaviour {
+
+    public MeshRenderer[] skins_meshes;
+    public SkinnedMeshRenderer[] skins_skinnedMeshes;
+
+    public int index = -1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < skins_meshes.Length; i++)
+        {
+            skins_meshes[i].enabled = (i == index) ? true : false;
+        }
+
+        for (int i = 0; i < skins_skinnedMeshes.Length; i++)
+        {
+            skins_skinnedMeshes[i].enabled = (i == (index - skins_meshes.Length)) ? true : false;
+        }
+	}
+}
---
+++ Assets/Scripts/SurvivalSumo2/SyncAnimPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncAnimPlayer : MonoBehaviour {
+
+    //Animators de todos los objetos de skin
+    public Animator[] animators;
+
+    // Vars de estado de los animators
+    public int Piernas; 
+    public int Brazos;
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        foreach (Animator a in animators)
+        {
+            a.SetInteger("Piernas", Piernas);
+            a.SetInteger("Brazos", Brazos);
+        }
+
+    }
+
+}
